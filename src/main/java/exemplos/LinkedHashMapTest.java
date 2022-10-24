869d53950dedb7e0dbc13e4efab0ab029ff001d5
José Anderson <jose.andersonjr2@gmail.com>
2022-10-24 19:39:29 -0300

    Add exercicios LinkedHashSet, LinkedHashMap

--- src/main/java/exemplos/LinkedHashMapTest.java
+++ src/main/java/exemplos/LinkedHashMapTest.java
@@ -3,7 +3,7 @@ package exemplos;
 import java.util.HashMap;
 import java.util.Map;
 
-public class HashMapTest { // Não podem haver chaves duplicadas
+public class LinkedHashMapTest {
     public static void main(String[] args) {
         Map<String, String> map = new HashMap<>(); // Não tem ordem específica no retorno dos dados
         map.put("teklado", "teclado");
